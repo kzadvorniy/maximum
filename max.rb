ee8303a702f8a8eb01c95d927fac1f4804305bf3
kzadvorniy <kir594793@mail.ru>
2012-12-10 17:55:24 +0400

    Update max.rb

--- max.rb
+++ max.rb
@@ -3,13 +3,9 @@ b = 423
 c = 5
 
 if a > b && a > c
-	puts "Maximum number is a = #{a}"
-else 
-	if b > a && b > c
-		puts "Maximum number is b = #{b}"
-	else 
-		if c > a && c > b
-			puts "Maximum number is c = #{c}"
-end
-end
+  puts "Maximum number is a = #{a}" 
+elsif b > a && b > c
+  puts "Maximum number is b = #{b}"
+elsif c > a && c > b
+  puts "Maximum number is c = #{c}"
 end
